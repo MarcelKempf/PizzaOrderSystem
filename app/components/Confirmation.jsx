ac8b536fa9da8b87ebd7b65ed466595c263e58c9
MarcelKempf <marcel@familie-kempf.net>
2019-05-21 17:36:52 +1000

    Init Component

---
+++ app/components/Confirmation.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Confirmation extends Component {
+
+  render(){
+    return(
+
+        <h1>Confirmation</h1>
+
+    );
+  }
+
+}
+
+export default Confirmation;
