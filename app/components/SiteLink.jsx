d1be6af04a955494a2bb97d6651af5bd811fda3a
MarcelKempf <marcel@familie-kempf.net>
2019-05-21 17:36:09 +1000

    added Linkbuilder

---
+++ app/components/SiteLink.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Icon } from 'react-materialize';
+
+const buildLink = (name, index, icon) => {
+  return (
+    <Link className={'link_' + name} key={index} to={'/' + (name == 'Home' ? '' : name.toLowerCase())}>{icon != null ? <Icon className="tag_name">{icon}</Icon> : ''}{name}</Link>
+  );
+};
+
+export { buildLink };
