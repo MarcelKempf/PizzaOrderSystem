8e817240dfcdc4252568a6e09c1378293c21dcae
MarcelKempf <marcel@familie-kempf.net>
2019-05-21 17:43:51 +1000

    Init Component

---
+++ app/components/Creator.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Creator extends Component {
+
+  render(){
+    return(
+
+        <h1>Creator</h1>
+
+    );
+  }
+
+}
+
+export default Creator;
