c897cbb4acb635205e49c9fb6db9ab0b8f3ba2c3
MarcelKempf <marcel@familie-kempf.net>
2019-06-06 20:30:33 +1000

    Path changed for gh-pages

--- app/components/Errorpage.jsx
+++ app/components/Errorpage.jsx
@@ -8,7 +8,7 @@ class Errorpage extends Component {
       <div className="information_box">
         <h2 className="information_title">404 Error</h2>
         <p>We couldn't find your page</p>
-        <Button className="back_home"waves="light" node="a" href="/">
+        <Button className="back_home"waves="light" node="a" href="PizzaOrderSystem/">
           Go to homepage
         </Button>
       </div>
