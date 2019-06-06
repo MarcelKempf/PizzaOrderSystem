edb94460080e9153b5af46677fa2c985ef1e0dc3
MarcelKempf <marcel@familie-kempf.net>
2019-06-06 20:33:05 +1000

    Path changed for gh-pages

--- app/components/SiteLink.jsx
+++ app/components/SiteLink.jsx
@@ -4,7 +4,7 @@ import { Icon } from 'react-materialize';
 
 const buildLink = (name, index, icon) => {
   return (
-    <Link className={'link_' + name} key={index} to={'PizzaOrderSystem/' + (name == 'Home' ? '' : name.toLowerCase())}>{icon != null ? <Icon className="tag_name">{icon}</Icon> : ''}{name}</Link>
+    <Link className={'link_' + name} key={index} to={'/PizzaOrderSystem/' + (name == 'Home' ? '' : name.toLowerCase())}>{icon != null ? <Icon className="tag_name">{icon}</Icon> : ''}{name}</Link>
   );
 };
 
