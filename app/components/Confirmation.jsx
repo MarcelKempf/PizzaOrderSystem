9278d792f4b7ff6da33b945810ed8ae4db001aee
MarcelKempf <marcel@familie-kempf.net>
2019-06-14 15:59:35 +1000

    formatter excluded

--- app/components/Confirmation.jsx
+++ app/components/Confirmation.jsx
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { CheckoutCart } from './CheckoutCart';
 
-class Confirmation extends Component {
-
-  render(){
-    return(
-
-        <h1>Confirmation</h1>
-
-    );
+export default function Confirmation(props) {
+  const formatter = () => {
+    return new Intl.NumberFormat('en-AU', {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    });
   }
-
+  const { payment, shoppingCart } = props.location.state;
+  return(
+    <div className="confirmation">
+      <h1 className="payment_state">{payment.paid === true ? 'Thank you for your payment': 'Payment unsuccessful!' }</h1>
+      {payment.paid === true && shoppingCart.length !== 0 ?
+          <CheckoutCart isOnlyDisplay history={props.history} value={{ 'shoppingCart': shoppingCart, 'currencyFormatter': formatter}} />
+          : <Link className='link_tryagain' to={'/creator'}>Try again</Link>
+        }
+    </div>
+  );
 }
-
-export default Confirmation;
